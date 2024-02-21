f677672d00455af3c783f978aa96761f90375fad
Jorge Henrique <jorgehenriquejds@gmail.com>
2024-02-20 22:30:05 -0300

    crm-2: creating page login

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Models\User;
+
+use function Pest\Laravel\actingAs;
+
 it('returns a successful response', function () {
+    $user = User::factory()->create();
+    actingAs($user);
     $response = $this->get('/');
 
     $response->assertStatus(200);
